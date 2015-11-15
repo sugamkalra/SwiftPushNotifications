af01cc060d432545db78d34a487bbbc6b715ac5c
Sugam Kalra <sugamkalra@sugams-macbook-pro.local>
2015-11-16 04:17:59 +0530

    Initial Commit

---
+++ SwiftyPushNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyPushNotifications
+//
+//  Created by Sugam Kalra on 16/11/15.
+//  Copyright © 2015 Sugam Kalra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
